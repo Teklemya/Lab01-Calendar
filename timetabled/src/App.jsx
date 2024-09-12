9773cab5b2b5d2d9c29df89533da54fef771b97f
Teklemya <Teklemya@mail.uc.edu>
2024-09-12 10:32:42 -0400

    inital commit

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Calendar from './Components/Calendar'
+
+const App = () => {
+
+  return (
+   <div className="App">
+    <h1>A Week in the Afterlife!</h1>
+    <h2>A Week in the Afterlife: Where Bureaucracy, Hovering Lessons, Awkward Encounters with Historical Celebrities and Bad Karaoke Never Die!</h2>
+    <Calendar />
+   </div>
+  )
+}
+
+export default App
---
+++ timetabled/src/Components/Calendar.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <div>Testing the calendar component</div>
+        </div>
+    )
+}
+export default Calendar;
