82176de9cb8225f538e7044e470145a32d8837cb
Teklemya <Teklemya@mail.uc.edu>
2024-09-12 11:17:56 -0400

    formated

--- timetabled/src/Components/Calendar.jsx
+++ timetabled/src/Components/Calendar.jsx
@@ -2,35 +2,61 @@
 import React from "react";
 
 const Calendar = () => {
-    return (
-        <div className="Calendar">
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Sunday</th>
-                        <th>Monday</th>
-                        <th>Tuesday</th>
-                        <th>Wednesday</th>
-                        <th>Thursday</th>
-                        <th>Friday</th>
-                        <th>Saturday</th>                      
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr><td className="time">8:00 am</td></tr>
-                   <tr> <td className="time">9:00 am</td></tr>
-                   <tr> <td className="time">10:00 am</td></tr>
-                   <tr> <td className="time">11:00 am</td></tr>
-                   <tr> <td className="time">12:00 pm</td></tr>
-                   <tr> <td className="time">1:00 pm</td></tr>                 
-                   <tr> <td className="time">2:00 pm</td></tr>                      
-                    <tr><td className="time">3:00 pm</td></tr>                        
-                   <tr><td className="time">4:00 pm</td></tr>                  
-                    <tr><td className="time">5:00 pm</td></tr>                   
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="Calendar">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="time">8:00 am</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">9:00 am</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">10:00 am</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">11:00 am</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">12:00 pm</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">1:00 pm</td>
+          </tr>
+          <tr>
+            {" "}
+            <td className="time">2:00 pm</td>
+          </tr>
+          <tr>
+            <td className="time">3:00 pm</td>
+          </tr>
+          <tr>
+            <td className="time">4:00 pm</td>
+          </tr>
+          <tr>
+            <td className="time">5:00 pm</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 export default Calendar;
