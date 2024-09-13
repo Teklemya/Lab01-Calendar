c70030e872e497e52cc2c6602f97b9a46a82793f
Teklemya <Teklemya@mail.uc.edu>
2024-09-12 23:33:37 -0400

    finished the props

--- timetabled/src/Components/Event.jsx
+++ timetabled/src/Components/Event.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 
-const Event = ({color, event,}) => {
+const Event = ({color, event, location}) => {
   return (
     <td className={'Event ' + color}>
         <h5>{event}</h5>
@@ -12,6 +12,7 @@ const Event = ({color, event,}) => {
 Event.propTypes = {
     color: PropTypes.string.isRequired,
     event: PropTypes.string.isRequired,
+    location: PropTypes.string,
   };
 
 export default Event
