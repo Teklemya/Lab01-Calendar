2c41516380497b3f66974e7a0610bf7d196517ad
Teklemya <Teklemya@mail.uc.edu>
2024-09-12 23:33:12 -0400

    moved calndar

--- timetabled/src/Components/Calendar.jsx
+++ timetabled/src/Components/Calendar.jsx
@@ -19,95 +19,98 @@ const Calendar = () => {
         <tbody>
           <tr>
             <td className="time">8:00 am</td>
-            <Event event='Coffee with God' color ='green' location='Maple & Ash'/>
+            <Event
+              event="Coffee with God"
+              color="green"
+              location="Maple & Ash"
+            />
             <td></td>
             <td></td>
             <td></td>
             <td></td>
-            <Event event='Fancy Dinner 🎩' color ='green'/>
+            <Event event="Fancy Dinner 🎩" color="green" />
             <td></td>
-            
           </tr>
           <tr>
             {" "}
             <td className="time">9:00 am</td>
             <td></td>
-             <Event event='Meet & Greet ' color ='green'/>
-             <td></td>
+            <Event event="Meet & Greet " color="green" />
             <td></td>
-            <Event event='New Cloud' color ='green'/>
             <td></td>
-           
+            <Event event="New Cloud" color="green" />
+            <td></td>
             <td></td>
           </tr>
           <tr>
             {" "}
             <td className="time">10:00 am</td>
-            
-            <td></td><Event event='Reincarnation Lottery' color ='pink'/>
+            <td></td>
+            <Event event="Reincarnation Lottery" color="pink" />
             <td></td>
             <td></td>
             <td></td>
-            <Event event='Service Complaints' color ='blue'/>
+            <Event event="Service Complaints" color="blue" />
             <td></td>
           </tr>
           <tr>
             {" "}
             <td className="time">11:00 am</td>
-            
             <td></td>
-            <td></td><Event event='Ghost Pranks' color ='blue'/>
-            <td></td><Event event='Levitation 101' color ='pink'/>
             <td></td>
-            
+            <Event event="Ghost Pranks" color="blue" />
+            <td></td>
+            <Event event="Levitation 101" color="pink" />
+            <td></td>
             <td></td>
           </tr>
           <tr>
             {" "}
             <td className="time">12:00 pm</td>
-            <Event event='Escape Room' color ='green'/>
+            <Event event="Escape Room" color="green" />
             <td></td>
             <td></td>
             <td></td>
             <td></td>
-            
-            <td></td><Event event='Debate Socrates' color ='blue'/>
+            <td></td>
+            <Event event="Debate Socrates" color="blue" />
           </tr>
           <tr>
             {" "}
             <td className="time">1:00 pm</td>
-            
             <td></td>
-            <td></td><Event event='Cosmic Karaoke' color ='pink'/>
-            <td></td><Event event='Speed Dating' color ='pink'/>
             <td></td>
-            
+            <Event event="Cosmic Karaoke" color="pink" />
+            <td></td>
+            <Event event="Speed Dating" color="pink" />
+            <td></td>
             <td></td>
           </tr>
           <tr>
             {" "}
             <td className="time">2:00 pm</td>
-            
-            <td></td>
-            <td></td><Event event='Astral Travel' color ='green'/>
             <td></td>
             <td></td>
-            <Event event='Anger Management' color ='pink'/>
+            <Event event="Astral Travel" color="green" />
+            <td></td>
+            <td></td>
+            <Event event="Anger Management" color="pink" />
             <td></td>
           </tr>
           <tr>
             <td className="time">3:00 pm</td>
-            <Event event='Relaxation on Cloud 9' color ='green'/>
+            <Event event="Relaxation on Cloud 9" color="green" />
             <td></td>
             <td></td>
             <td></td>
             <td></td>
-            
-            <td></td><Event event='Afterlife’s Got Talent' color ='blue'/>
+
+            <td></td>
+            <Event event="Afterlife’s Got Talent" color="blue" />
           </tr>
           <tr>
             <td className="time">4:00 pm</td>
-            <Event event='Coffee with Elvis' color ='blue'/>
+            <Event event="Coffee with Elvis" color="blue" />
             <td></td>
             <td></td>
             <td></td>
@@ -116,12 +119,14 @@ const Calendar = () => {
           </tr>
           <tr>
             <td className="time">5:00 pm</td>
-            
-            <td></td><Event event='Meet Cerberus' color ='pink'/>
+
+            <td></td>
+            <Event event="Meet Cerberus" color="pink" />
             <td></td>
             <td></td>
-            <Event event='Walking Through Walls' color ='blue'/>
+
             <td></td>
+            <Event event="Walking Through Walls" color="blue" />
           </tr>
         </tbody>
       </table>
